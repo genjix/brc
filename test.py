775af0546cc6a73899c70553fe29d4fb5112f2d0
Amir Taaki <genjix@riseup.net>
2013-10-14 02:59:02 +0200

    added better comments to test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ def newtx(tx_hash):
 
 b = Broadcaster()
 b.start(1, 10, 4, newtx, started)
+# To broadcast a tx use:
+#   b.broadcast(raw_tx)
+# Where raw_tx is the raw tx data bytes.
 raw_input()
 # You must stop otherwise exception is thrown.
 b.stop()
